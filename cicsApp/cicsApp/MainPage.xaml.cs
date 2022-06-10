9c89195f338e23d187bd67c5cc54c3dc5e4bcb7e
demo2 <sendingcode123@gmail.com>
2022-06-10 15:52:36 +0630

    プロジェクト ファイルを追加します。

---
+++ cicsApp/cicsApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace cicsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            List<Repository> repositories = await _restService.GetRepositoriesAsync(Constants.GitHubReposEndpoint);
+            collectionView.ItemsSource = repositories;
+        }
+    }
+}
